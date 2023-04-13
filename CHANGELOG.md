1a01b2ca6c1e56aedd7140ccdece08493029e3de
Christoph Lettau <nicolas62974@gmail.com>
2023-04-13 11:19:18 +0200

    Now you can type something in.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # NShell Changelog
 
+## Build 2
+
+- Now you can type something in, but then it just says goodbye.
+
 ## Build 1
 
 - Added build information
--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
-#include <array>
 #include <iostream>
 #include <limits>
-#include <span>
 
-int buildNr = 1;
+int buildNr = 2;
 
 int main()
 {
     std::cout << "NShell" << '\n';
     std::cout << "Build: " << buildNr << '\n';
     std::cout << "Early prototype! Don't expect it to be useful." << '\n';
+
+    std::cout << "NShell > ";
+    std::string command_to_run;
+    std::cin >> command_to_run;
+
+    std::cout << "You would have run " << command_to_run << ". Thanks for testing. Goodbye.";
 
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
