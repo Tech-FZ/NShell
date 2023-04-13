d8009eeb2a07c9bb4831893da80917763e54deb6
Christoph Lettau <nicolas62974@gmail.com>
2023-04-13 11:41:12 +0200

    Now you can get to know the version and exit properly.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # NShell Changelog
 
+## Build 3
+
+- Okay, now version and exit do something.
+
 ## Build 2
 
 - Now you can type something in, but then it just says goodbye.
--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <limits>
 
-int buildNr = 2;
+int buildNr = 3;
 
 int main()
 {
     std::cout << "NShell" << '\n';
     std::cout << "Build: " << buildNr << '\n';
-    std::cout << "Early prototype! Don't expect it to be useful." << '\n';
+    std::cout << "Early prototype! Don't expect it to be useful.\n" << '\n';
 
-    std::cout << "NShell > ";
-    std::string command_to_run;
-    std::cin >> command_to_run;
+    while (true) {
+        std::cout << "NShell > ";
+        std::string command_to_run;
+        std::cin >> command_to_run;
 
-    std::cout << "You would have run " << command_to_run << ". Thanks for testing. Goodbye.";
+        if (command_to_run == "version") {
+            std::cout << "NShell Build " << buildNr << '\n';
+        }
+
+        else if (command_to_run == "exit") {
+            std::cout << "Goodbye. Please press Enter to exit the application." << '\n';
+            break;
+        }
+
+        else {
+            std::cout << "Unknown command!\n";
+        }
+    }
 
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
